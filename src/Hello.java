f1a4be91ea93942abecc21d515a0b8cf035e8ba4
Emiliano B <emilianoberrones@gmail.com>
2018-08-25 18:06:48 -0500

    first commit

---
+++ src/Hello.java
@@ -0,0 +1,16 @@
+public class Hello {
+    public static void main(String[] args) {
+
+        System.out.println("Hello, Emiliano!");
+
+        int myFirstNumber = (10 + 5) + (2 * 10);
+        int mySecondNumber = 12;
+        int myThirdNumber = myFirstNumber + 2;
+
+        int myTotal = myFirstNumber + mySecondNumber + myThirdNumber;
+
+        int myLastOne = myTotal - 1000;
+
+        System.out.println(myLastOne);
+    }
+}
